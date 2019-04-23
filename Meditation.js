e6725827bf621ed1d34f33fe9b96b2fdaa62785a
DoanThomas88 <38048770+Techtronic88@users.noreply.github.com>
2019-04-24 02:31:20 +0700

    Add files via upload
    Just finished the CSS, laid out requirement list for JS

--- Meditation.js
+++ Meditation.js
@@ -1,1 +1,13 @@
-<script>   </script>
+// WHAT DO YOU NEED TO DO 
+// 1st: Play button click gets played
+// 2nd: Click 2 Mins - 5 Mins - 10 Mins - Time start 
+     //2:B Count Down timer needs to display the time left
+     // 2:C Click multiple time button will get conflic results - need to clear queu
+
+
+// 3rdL Click Rains Button - Play Rain sounds
+    // 3a: Video background change to Rain
+
+
+// 4TH: Click Sunny Button - Play Sunny Sounds
+    // 4a: Video Background Change to  Sunny
